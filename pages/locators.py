27e5a439116df4234e94fa9ad6f570799939d88a
ninocriado <nastya_potekhina@mail.ru>
2022-09-18 00:56:44 +0300

    Add class LoginPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,13 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_BUTTON = (By.ID, "login_link")
+    EMAIL_REGISTRATION = (By.NAME, "registration-email")
+    PASSWORD1_REGISTRATION = (By.NAME, "registration-password1")
+    PASSWORD2_REGISTRATION = (By.NAME, "registration-password2")
+    BUTTON_REGISTRATION = (By.NAME, "registration_submit")
+    EMAIL_LOGIN = (By.NAME, "login-username")
+    PASSWORD_LOGIN = (By.NAME, "login-password")
+    LOGIN_SUBMIT = (By.NAME, "login_submit")
---
+++ pages/login_page.py
@@ -0,0 +1,23 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert self.browser.get(self.url), "Wrong URL"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.EMAIL_LOGIN), "Wrong login"
+        assert self.is_element_present(*LoginPageLocators.PASSWORD_LOGIN),"Wrong password"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_SUBMIT), "Wrong login or password"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.EMAIL_REGISTRATION), "Wrong email"
+        assert self.is_element_present(*LoginPageLocators.PASSWORD1_REGISTRATION), "Password field is not present"
+        assert self.is_element_present(*LoginPageLocators.PASSWORD2_REGISTRATION), "Password field is not present"
+        assert self.is_element_present(*LoginPageLocators.BUTTON_REGISTRATION), "Button registration is not present"
